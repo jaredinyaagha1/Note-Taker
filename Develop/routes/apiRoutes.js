23a7f7bac94bbf80f73a6ba722e675b74949969c
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-01-17 22:16:28 -0500

    created post route in apiRoutes.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,14 @@
 const controllers = require('../db/controllers');
+const router = require('./htmlRoutes');
 
 router.get('/notes', (req, res) => {
     controllers.getNotes().then(notes => {
+        return res.json(notes)
+    })
+})
+
+router.post('/notes', (req, res) => {
+    controllers.addNote().then(notes => {
         return res.json(notes)
     })
 })
